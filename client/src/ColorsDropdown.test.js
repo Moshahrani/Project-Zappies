9475a127e6ed419d7b7541438c35553865f4eb75
Moshfeq <moshfeqshahrani@gmail.com>
2021-12-19 21:37:30 -0800

    Created test for Colors Component, fixed module destination errors, fixed global mount issues for jest

---
+++ client/src/ColorsDropdown.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ColorsDropdown from './ColorsDropdown.jsx';
+
+it("renders Colors ", () => {
+    const wrapper = shallow(<ColorsDropdown />);
+    const colorsDisplay = "Colors";
+    expect(wrapper.contains(colorsDisplay)).toEqual(true);
+})
