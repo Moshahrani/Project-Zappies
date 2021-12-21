a407b3ef61ea37465c6097f393c12bc2bd3f7221
Moshfeq <moshfeqshahrani@gmail.com>
2021-12-21 09:29:46 -0800

    Added Initial tests for SocialMedia Component

--- client/src/SocialMedia.test.js
+++ client/src/SocialMedia.test.js
@@ -1,5 +1,15 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { SocialIcon } from 'react-social-icons';
 import SocialMedia from './SocialMedia.jsx';
 
+
+it("renders without issues", () => {
+    shallow(<SocialMedia />);
+})
+
+it("renders Socia Media without errors", () => {
+    const wrapper = shallow(<SocialMedia />);
+    const socials = "Social Media";
+    expect(wrapper.contains(socials)).toEqual(true);
+})
