e3ce86fd18173aa8ed89339c8836d7e117c840ef
Moshfeq <moshfeqshahrani@gmail.com>
2022-01-02 22:16:05 -0800

    added css file for PhotoGallery component

--- client/src/PhotoGallery.jsx
+++ client/src/PhotoGallery.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import ProductDetail from './ProductDetail.jsx'
+import './PhotoGallery.css';
 
 class PhotoGallery extends React.Component {
     constructor(props) {
@@ -14,19 +15,42 @@ class PhotoGallery extends React.Component {
     }
 
     
+
+    // rightArrowClick = (event) => {
+
+    // }
+
+    // leftArrowClick = (event) => {
+
+    // }
+
+
     render() {
         let images = [];
+        let firstIndex = 0;
+        let lastIndex = null;
         if(this.props.imageList) {
           images = [...this.props.imageList]
+          lastIndex = this.props.imageList.length - 1;
         }
-
+       console.log(images[this.state.currentIndex])
+        let image = images[this.state.currentIndex]
         
-        return ( <div className="currentImage">
-                <img id="img" src={images[this.state.currentIndex]} ></img>
-    
+        return (<div className="carousel">
+            <div 
+            className="carouselInner" 
+            style={{ backgroundImage: `url(${image})` }}
+            >
             </div>
+            </div> 
         ) 
     }
 }
 export default PhotoGallery;
 
+
+{/* <div 
+            className="carouselInner">
+                <img id="img" src={images[this.state.currentIndex]} ></img>
+            </div>
+            </div> */}
