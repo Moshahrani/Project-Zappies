839d63c30a432c89393be8430a9c7eb4a91b5beb
Moshfeq <moshfeqshahrani@gmail.com>
2022-01-24 19:13:16 -0800

    added logic for percentage calculations of each rating and number of ratins for a range of 1-5 in rating options

--- client/src/RatingBar.jsx
+++ client/src/RatingBar.jsx
@@ -4,14 +4,32 @@ import './styles.css';
 
 function RatingBar(props) {
 
+  let ratings = props.ratings;
+  let totalRatings = 0;
+ 
+  for (let i in ratings) {
+      let num = Number(ratings[i]);
+      ratings[i] = num;
+      totalRatings += num;
+  }
+
+  for (let i in ratings) {
+       ratings[i] = String(100 * (ratings[i] / totalRatings)) + '%';
+  }
+  console.log(ratings)
   
+  let percent = '50%';
+
     return (
 
         <div className="container">
+            Ratings
             <div className="bar">
-                <div className="bar-inner" style={{width : '50%'}} />
-                <div className="bar-inner" style={{width : '30%'}} />
-                <div className="bar-inner" style={{width : '20%'}} />
+                <div className="bar-inner" style={{width : ratings[5]}} />
+                <div className="bar-inner" style={{width : ratings[4]}} />
+                <div className="bar-inner" style={{width : ratings[3]}} />
+                <div className="bar-inner" style={{width : ratings[2]}} />
+                <div className="bar-inner" style={{width : ratings[1]}} />
             </div>
             
         </div>
