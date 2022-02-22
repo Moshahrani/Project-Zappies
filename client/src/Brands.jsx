bc58f8603136393f58ae8cd96586638e35b6691a
Moshfeq <moshfeqshahrani@gmail.com>
2022-02-22 11:29:28 -0800

    added new Brands functional component

---
+++ client/src/Brands.jsx
@@ -0,0 +1,13 @@
+import { ConstructionRounded } from '@mui/icons-material';
+import React from 'react'
+import ReactDom from 'react-dom';
+
+function Brands(props) {
+   
+
+    // return (
+
+    // );
+}
+
+export default Brands;
