c4ce27106f17a82e029aa02d6f990e491c315f13
Moshfeq <moshfeqshahrani@gmail.com>
2022-04-17 16:49:30 -0700

    changed port number to 3000 and formatted extra space between code

--- Server/index.js
+++ Server/index.js
@@ -3,8 +3,7 @@ let app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
 
-
-let port = 2000;
+let port = 3000;
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
