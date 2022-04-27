418ff5d6151dbe094955f769158663d0515cc58e
Moshfeq <moshfeqshahrani@gmail.com>
2022-04-26 21:30:28 -0700

    formatted RatingBar

--- client/src/RatingBar.jsx
+++ client/src/RatingBar.jsx
@@ -9,7 +9,7 @@ function RatingBar(props) {
     let reviewCount = {}
     let totalRatings = 0;
     let numRatings = [];
-    
+
     for (let i in ratings) {
         let num = Number(ratings[i]);
         ratings[i] = num;
@@ -17,10 +17,11 @@ function RatingBar(props) {
         totalRatings += num;
         numRatings.push(num)
     }
-    
+
     for (let i in ratings) {
         ratings[i] = String(Math.round(100 * (ratings[i] / totalRatings)));
     }
+
     let number = props.ratings
 
     return (
@@ -32,27 +33,27 @@ function RatingBar(props) {
                 <div className="ratings-one" >
                     <span className="rating">5</span>
                     <span className="totalRatings">{props.ratings[5]}</span>
-                    <div className="ratings" style={{ width: (ratings[5] + "%")}}></div>
+                    <div className="ratings" style={{ width: (ratings[5] + "%") }}></div>
                 </div>
                 <div className="ratings-two" >
                     <span className="rating">4</span>
                     <span className="totalRatings">{reviewCount[4]}</span>
-                    <div className="ratings" style={{ width: (ratings[4] + "%")}}></div>
+                    <div className="ratings" style={{ width: (ratings[4] + "%") }}></div>
                 </div>
                 <div className="ratings-three" >
                     <span className="rating">3</span>
                     <span className="totalRatings">{reviewCount[3]}</span>
-                    <div className="ratings" style={{ width: (ratings[3] + "%")}}></div>
+                    <div className="ratings" style={{ width: (ratings[3] + "%") }}></div>
                 </div>
                 <div className="ratings-four" >
                     <span className="rating">2</span>
                     <span className="totalRatings">{reviewCount[2]}</span>
-                    <div className="ratings" style={{ width: (ratings[2] + "%")}}></div>
+                    <div className="ratings" style={{ width: (ratings[2] + "%") }}></div>
                 </div>
                 <div className="ratings-five" >
                     <span className="rating">1</span>
                     <span className="totalRatings">{reviewCount[1]}</span>
-                    <div className="ratings" style={{ width: (ratings[1] + "%")}}></div>
+                    <div className="ratings" style={{ width: (ratings[1] + "%") }}></div>
                 </div>
             </div>
         </div>
