87a1132481ea5112cb2e3b4fae0e830753229696
Moshfeq <moshfeqshahrani@gmail.com>
2022-09-09 13:39:54 -0700

    mui icon update in reviews component

--- client/src/Reviews.js
+++ client/src/Reviews.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import StarRatings from './StarRatings.js';
 
 function Reviews(props) {
