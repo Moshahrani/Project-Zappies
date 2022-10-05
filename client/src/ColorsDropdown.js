b1687da720ef3bc99d3b77209db4c47d4b5fb111
Moshfeq <moshfeqshahrani@gmail.com>
2022-10-05 11:28:16 -0700

    replaced dropdown logic which disallowed dropdown actions from occuring

--- client/src/ColorsDropdown.js
+++ client/src/ColorsDropdown.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
 
 class ColorsDropdown extends React.Component {
   constructor(props) {
@@ -16,34 +16,93 @@ class ColorsDropdown extends React.Component {
     event.preventDefault();
 
     this.setState({ showMenu: true }, () => {
-      document.addEventListener('click', this.closeMenu);
+      document.addEventListener("click", this.closeMenu);
     });
   }
 
   closeMenu() {
     this.setState({ showMenu: false }, () => {
-      document.removeEventListener('click', this.closeMenu);
+      document.removeEventListener("click", this.closeMenu);
     });
   }
 
   render() {
-    return (
-      <div className="dropdown" style={{ width: "200px" }} >
-        <div className="button" onClick={this.showMenu}> Colors </div>
-        {this.state.showMenu ? (
-          <ul id="dd">
-            {this.props.colors.map((item, index) => (
-              <li value={item} onClick={this.props.printShoe} key={index}>{item}</li>
-            ))}
-          </ul>
-        )
-          :
-          (
-            null
-          )
-        }
-      </div>
-    )
+    if (!this.state.showMenu) {
+      return (
+        <div id="menu" className="colorsButton" onClick={this.showMenu}>
+          Colors
+        </div>
+      );
+    } else {
+      return (
+        <div className="ddList">
+          {this.state.showMenu ? (
+            <div className="ddContent">
+              {this.props.colors.map((item, index) => (
+                <p className="dd-color-item" key={index}>
+                  <button
+                    type="button"
+                    id="printShoeProp"
+                    value={item}
+                    onClick={this.props.printShoe}
+                    key={index}
+                  >
+                    <span>{item}</span>
+                  </button>
+                </p>
+              ))}
+            </div>
+          ) : null}
+        </div>
+      );
+    }
   }
 }
 export default ColorsDropdown;
+
+// if (!this.state.showMenu) {
+//   return (
+//     <div id="menu" className="colorsButton" onClick={this.showMenu}>
+//       Colors
+//     </div>
+//   )
+// } else {
+//   return (<div className="ddList">
+//     {
+//       this.state.showMenu
+//         ? (
+//           <div className="ddContent">
+//             {this.props.colors.map((item, index) => (
+//               <p className="dd-color-item" key={index}>
+//                 <button type="button" id="printShoeProp" value={item} onClick={this.props.printShoe} key={index}>
+//                   <span>{item}</span>
+//                 </button>
+//               </p>
+//             ))}
+//           </div>
+//         )
+//         : (
+//           null
+//         )
+//     }
+//   </div>
+//   )
+// }
+
+//  return (
+//   <div className="dropdown" style={{ width: "200px" }} >
+//     <div className="button" onClick={this.showMenu}> Colors </div>
+//     {this.state.showMenu ? (
+//       <ul id="dd">
+//         {this.props.colors.map((item, index) => (
+//           <li value={item} onClick={this.props.printShoe} key={index}>{item}</li>
+//         ))}
+//       </ul>
+//     )
+//       :
+//       (
+//         null
+//       )
+//     }
+//   </div>
+// )
